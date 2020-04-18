38f75a111cf49c1770c24392cabdef6f2c833539
carlosmstelles <carlosmstelles@gmail.com>
2020-04-18 17:52:07 -0300

    capitulo 3

---
+++ QuickBuy.Dominio/Contratos/IbaseRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Contratos
+{
+    public interface IbaseRepositorio<TEntity> : IDisposable where TEntity : class
+    {
+        void Adicionar(TEntity entity);
+        void Atualizar(TEntity entity);
+        TEntity ObterPorId(int id);
+        IEnumerable<TEntity> ObterTodos();
+        void Remover(TEntity entity);
+    }
+}
---
+++ QuickBuy.Repositorio/Config/PedidoConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.Entidades;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class PedidoConfiguration : IEntityTypeConfiguration<Pedido>
+    {
+        public void Configure(EntityTypeBuilder<Pedido> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.DataPedido).IsRequired();
+            builder.Property(p => p.UsuarioId).IsRequired();
+            builder.Property(p => p.DataPrevisaoEntrega);
+            builder.Property(p => p.CEP).IsRequired().HasMaxLength(9);
+            builder.Property(p => p.Estado).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.Cidade).IsRequired().HasMaxLength(50);
+            builder.Property(p => p.EnderecoCompleto).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.NumeroEndereco).IsRequired().HasMaxLength(10);
+
+        }
+    }
+}
